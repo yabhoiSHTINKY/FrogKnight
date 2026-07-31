0f2b50c0312b1046017441705e9878e567821088
TotalSync <1an6reen@gmail.com>
2026-07-31 08:08:23 -0500

    Corrected Pile Driver to lok for new debuff ContfrogPower

--- FrogKnightCode/Cards/PileDriver.cs
+++ FrogKnightCode/Cards/PileDriver.cs
@@ -1,5 +1,7 @@
 using FrogKnight.FrogKnightCode.Cards;
+using FrogKnight.FrogKnightCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -15,7 +17,7 @@ public class PileDriver() : FrogKnightCard(2,
     public override string CustomPortraitPath => "res://FrogKnight/images/card_portraits/piledrive.png";
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        ..MakeCalculatedDamage(0, (card, target) => target?.GetPowerAmount<ConstrictPower>() ?? 0, 2)
+        ..MakeCalculatedDamage(0, (card, target) => target?.GetPowerAmount<ContfrogPower>() ?? 0, 2)
     ];
 
     protected override async Task OnPlay(
@@ -23,7 +25,7 @@ public class PileDriver() : FrogKnightCard(2,
         CardPlay play)
     {
         ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
-        await DamageCmd.Attack(base.DynamicVars.CalculatedDamage).FromCard(this).Targeting(play.Target)
+        AttackCommand attackCommand = await DamageCmd.Attack(base.DynamicVars.CalculatedDamage).FromCard(this).Targeting(play.Target)
             .WithHitFx("vfx/vfx_attack_blunt", null, "blunt_attack.mp3")
             .Execute(choiceContext);
     }
