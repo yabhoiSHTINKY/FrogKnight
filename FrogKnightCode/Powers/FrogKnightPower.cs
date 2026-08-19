11c800dfc08071902010da462d1b3eef7159dc25
TotalSync <1an6reen@gmail.com>
2026-08-18 19:46:17 -0500

    Updated Multiplayer Scaling to Default

--- FrogKnightCode/Powers/FrogKnightPower.cs
+++ FrogKnightCode/Powers/FrogKnightPower.cs
@@ -1,7 +1,10 @@
 ﻿using BaseLib.Abstracts;
 using BaseLib.Extensions;
 using FrogKnight.FrogKnightCode.Extensions;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
 
 namespace FrogKnight.FrogKnightCode.Powers;
 
@@ -23,9 +26,21 @@ public abstract class FrogKnightPower : CustomPowerModel
     public abstract override PowerType Type { get; }
 
     /// <summary>
+    /// Overriding Multiplayer Scaling to be true by default.
+    /// This can be overriden in specific cards if needed for balancing
+    /// </summary>
+    public override bool ShouldScaleInMultiplayer => true;
+
+    /// <summary>
     /// How this power stacks if reapplied. Counter is the most common type, where applying the power again just
     /// adds to the amount. Single means the power does not stack, like Barricade. None functions identically to
     /// Single, but you're suggested to use Single as it is more explicit about how it will work.
     /// </summary>
     public abstract override PowerStackType StackType { get; }
+
+    public override decimal GetScaledAmountForMultiplayer(ICombatState combatState, Creature? applier, decimal amount, Creature target,
+        CardModel? cardSource)
+    {
+        return (Decimal) ((combatState.Players.Count - 1) * 2 + 1) * amount;
+    }
 }
